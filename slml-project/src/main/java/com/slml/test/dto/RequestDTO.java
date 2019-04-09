3875f8ffc0eb8cae9d726ea601199e7d1425f10c
muneer <muneer9211@gmail.com>
2019-04-08 21:29:08 -0500

    I created new dto class file

---
+++ slml-project/src/main/java/com/slml/test/dto/RequestDTO.java
@@ -0,0 +1,27 @@
+package com.slml.test.dto;
+
+public class RequestDTO {
+
+	
+	protected Integer id;
+	
+	private String date;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	
+}
