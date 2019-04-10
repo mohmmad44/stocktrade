061fbaa131b446a81f823b141145bec85579beed
mohmmad1144 <mailtomohmmad@gmail.com>
2019-04-10 16:32:34 +0200

    changed the varible type in SQL 1.1.0 for timestamp

--- slml-project/src/main/resources/db/migration/V1.1.0__createTableScript.sql
+++ slml-project/src/main/resources/db/migration/V1.1.0__createTableScript.sql
@@ -2,7 +2,7 @@
 create table model_output(
 id integer not null auto_increment,
 date_Str varchar(30) not null,
-time_stamp TIMESTAMP not null,
+time_stamp varchar(30) not null,
 security_name varchar(255) not null,
 ISIN varchar(30) not null,
 yesterday_actual decimal(20,5) not null,
